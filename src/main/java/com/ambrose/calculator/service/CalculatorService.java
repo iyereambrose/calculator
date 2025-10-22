443daae72d35a686bf984216f6c2409538c1801f
Iyere Ambrose <iyereambrose60@gmail.com>
2025-10-22 13:52:29 +0100

    adding test

---
+++ src/main/java/com/ambrose/calculator/service/CalculatorService.java
@@ -0,0 +1,11 @@
+package com.ambrose.calculator.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalculatorService {
+
+    public int sum(int a, int b) {
+        return a + b;
+    }
+}
