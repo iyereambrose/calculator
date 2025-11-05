8528490b4d2d882ba83dba69075c8d9c58d55679
Iyere Ambrose <iyereambrose60@gmail.com>
2025-11-05 12:27:02 +0100

    modification

--- src/test/java/com/ambrose/calculator/CalculatorApplicationTests.java
+++ src/test/java/com/ambrose/calculator/CalculatorApplicationTests.java
@@ -7,8 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CalculatorApplicationTests {
